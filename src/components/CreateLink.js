c008258ad471ad43cccff8633fe821698443d07d
Ryan Bryce <ryan.bryce1992@gmail.com>
2019-06-09 19:06:12 -0600

    added routes

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -39,7 +39,11 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+        <Mutation 
+          mutation={POST_MUTATION}
+          variables={{ description, url }}
+          onCompleted={() => this.props.history.push('/')  
+        >
           {postMutation => {
             console.log(postMutation)
             return <button onClick={postMutation}>Submit</button>
